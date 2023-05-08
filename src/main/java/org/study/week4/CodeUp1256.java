4a4c48d15e66104be03175240357d2825bb5bc3d
sunga0101 <59547021+sunga0101@users.noreply.github.com>
2023-05-08 15:47:13 +0900

    [0508] 코드업 1256, 1287, 1361, 1367, 1671

---
+++ src/main/java/org/study/week4/CodeUp1256.java
@@ -0,0 +1,15 @@
+package org.study.week4;
+
+import java.util.Scanner;
+
+public class CodeUp1256 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int iCnt = sc.nextInt();
+        for (int i = 0; i < iCnt; i++) {
+            System.out.print("*");
+        }
+        System.out.print("\n");
+    }
+}
+
